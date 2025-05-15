bc63409fdb46d75a9873e3c11474a737b165e390
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-05-15 10:06:08 +0530

    url changed

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -4,9 +4,10 @@ export const environment = {
       // getVulnerability: "http://localhost:8080/cvss/getVulnerabilities",
       fetchVulnerability: "/cvss/getVulnerabilities",
       getVulnerabilities: "/cvss/vulnerabilities",
-      searchByKeyWordUrl: "http://localhost:8080/cvss/search/keywords?keywords=",
-      searchByCveid: "http://localhost:8080/cvss/search/cveId?cveId=",
-      searchByCpeName: "http://localhost:8080/cvss/search/cpe?cpe=",
+      searchByKeyWordUrl: "http://localhost:8080/cvss/search/cve/keywords?keywords=",
+      searchByCveid: "http://localhost:8080/cvss/search/cve/cveId?cveId=",
+      searchByCpeName: "http://localhost:8080/cvss/search/cve/cpeName?cpe=",
       apiKey: "ca987215-dbe8-42f0-a656-e5da368c3c70",
-      searchLikelyCpe: "http://localhost:8080/cvss/search/cpeName?cpeName="
+      searchLikelyKeyword: "http://localhost:8080/cvss/search/cpe/keyword?keyword=",
+      searchLikelyCpe: "http://localhost:8080/cvss/search/cpe/matchingCpeName?cpeName="
 }
