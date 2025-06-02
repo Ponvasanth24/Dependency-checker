6354967958f8f78b0a4fd0efc51a6abb6fb40f13
rammuthukumar1512 <muthukumar.ramasamy@isteer.com>
2025-06-02 12:40:56 +0530

    material added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation, withRouterConfig } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes,  withEnabledBlockingInitialNavigation(),
-      withRouterConfig({ onSameUrlNavigation: 'reload' })), provideHttpClient()]
+      withRouterConfig({ onSameUrlNavigation: 'reload' })), provideHttpClient(), importProvidersFrom(
+      BrowserAnimationsModule,
+      MatSidenavModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatListModule,
+      MatButtonModule
+    )]
 };
